52fe74abe0df45e861f25d3d248d6b4f4403f475
Shivaji Chalise <respond2shivaji@gmail.com>
2022-09-21 17:36:23 +0545

    Used Sanctum Authentication for routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -15,11 +15,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('articles', ArticleController::class);
-Route::get('/articles/search/{title}', [ArticleController::class, 'search']);
+/* Route::resource('articles', ArticleController::class); */
 
-/* Route::get('/articles', [ArticleController::class, 'index']); */
-/* Route::post('/articles', [ArticleController::class, 'store']); */
+// Public Routes
+Route::get('/articles/search/{title}', [ArticleController::class, 'search']);
+Route::get('/articles', [ArticleController::class, 'index']);
+Route::get('/articles/{id}', [ArticleController::class, 'show']);
+
+
+// Protected Routes
+Route::group(['middleware' => ['auth:sanctum']], function () {
+  Route::post('/articles', [ArticleController::class, 'store']);
+  Route::put('/articles/{id}', [ArticleController::class, 'update']);
+  Route::delete('/articles/{id}', [ArticleController::class, 'destroy']);
+});
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
   return $request->user();
