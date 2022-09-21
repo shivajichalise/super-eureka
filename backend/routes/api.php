75f6cefcd47942f83f1d063241e328684a8447e1
Shivaji Chalise <respond2shivaji@gmail.com>
2022-09-21 17:16:36 +0545

    Simple CRUD api created

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::resource('articles', ArticleController::class);
+
+/* Route::get('/articles', [ArticleController::class, 'index']); */
+/* Route::post('/articles', [ArticleController::class, 'store']); */
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+  return $request->user();
 });
