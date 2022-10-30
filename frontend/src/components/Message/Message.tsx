409c4cd280dd8eede7f6f05268ab3786db9cd26b
Shivaji Chalise <respond2shivaji@gmail.com>
2022-10-30 10:49:58 +0545

    Created flash message component

---
+++ frontend/src/components/Message/Message.tsx
@@ -0,0 +1,19 @@
+import {MessageProps} from '../../constants/Message.model'
+
+export const Message = (props: MessageProps) => {
+  const {message, type} = props
+  let color = 'orange-300'
+
+  color = (type === 'danger') ? 'bg-red-300'
+    : (type === 'warn') ? 'bg-orange-300'
+      : (type === 'success') ? 'bg-lightGreen'
+        : 'bg-indigo-300'
+
+  return (
+    <div className="container mx-auto px-7 flex justify-center items-center">
+      <div className={"rounded-lg p-2 " + color}>
+        <h1>{message} </h1>
+      </div>
+    </div>
+  )
+}
---
+++ frontend/src/constants/Message.model.ts
@@ -0,0 +1,4 @@
+export type MessageProps = {
+  message: string
+  type: string
+}
